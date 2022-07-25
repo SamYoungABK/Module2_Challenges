f3ac65149fa6ac72d8ffdad14de04be1e25de8ac
SamYoungABK <109538468+SamYoungABK@users.noreply.github.com>
2022-07-25 08:37:52 -0700

    Completed challenge1

---
+++ Challenge1/main.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+void GetValues(int&, int&);
+void OutputValues(int, int, int*, int*);
+
+int main()
+{
+	int a, b = 0;
+	int* ptrA = &a;
+	int* ptrB = &b;
+
+	GetValues(a, b);
+	OutputValues(a, b, ptrA, ptrB);
+}
+
+void GetValues(int& a, int& b)
+{
+	printf("Enter value for a:\n> ");
+	std::cin >> a;
+	printf("Enter value for b:\n> ");
+	std::cin >> b;
+	return;
+}
+
+void OutputValues(int a, int b, int* ptrA, int* ptrB)
+{
+	printf("\nA: %d\nB: %d\n", a, b);
+	printf("ptrA: %d\nptrB: %d\n", *ptrA, *ptrB);
+	return;
+}
